c4859fcb0144dd98ffe78f49994e434aec134403
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-10-24 12:48:02 +0530

    Create Find_mergepoint_ofTwo_linkedList.c

---
+++ Link List/Find_mergepoint_ofTwo_linkedList.c
@@ -0,0 +1,22 @@
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *p1= head1,*p2= head2;
+    while(p1 !=NULL){
+        p2 = head2;
+        while(p2 !=NULL  && (p1 != p2))
+            p2 = p2->next;
+        if(p1 == p2)
+            break;
+        p1 = p1->next;
+    }
+    return p1->data;
+}
