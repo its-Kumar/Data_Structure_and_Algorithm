89c47694981b366f2474997663f1977bb2423bb2
Kumar Shanu <KUMARSHANU1009@GMAIL.COM>
2020-04-09 20:16:20 +0530

    Stack implementaion using linkedList

---
+++ DataStructure.py/StackWithList.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
+
+class Stack(object):
+    def __init__(self, data):
+        self.head = None
+        if data:
+            for data in data:
+                self.push(data)
+
+    def push(self, data):
+        temp = Node(data)
+        temp.next = self.head
+        self.head = temp
+
+
+    def pop(self):
+        if self.head is None:
+            raise IndexError
+        temp = self.head
+        self.head = self.head.next 
+        return temp.data 
+
+    def peek(self):
+        if self.head is None:
+            raise IndexError
+        return self.head.data
+
+if __name__ == "__main__":
+    s = Stack(['first', 'second', 'third'])
+    print(s.pop())
+    print(s.pop())
