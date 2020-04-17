02e675f518f96ce4ebda0fb25fa55d6e18c95dd5
Kumar Shanu <KUMARSHANU1009@GMAIL.COM>
2020-04-17 19:44:57 +0530

    Graph Implementation

---
+++ DataStructure.py/Graph_adjLIST.py
@@ -0,0 +1,116 @@
+"""
+    Graph Adjacency List representation. 
+"""
+
+import sys
+
+
+class Vertex:
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+        # Set distance to infinity for all nodes
+        self.distance = sys.maxsize
+        # Mark all nodes unvisited
+        self.visited = False
+        # Predecessor
+        self.previous = None
+
+    def addNeighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
+
+    def getVertexID(self):
+        return self.id
+
+    def getConnections(self):
+        return self.adjacent.keys()
+
+    def getWeight(self, neighbor):
+        return self.adjacent[neighbor]
+
+    def setDistance(self, dist):
+        self.distance = dist
+
+    def getDistance(self):
+        return self.distance
+
+    def setPrevious(self, prev):
+        self.previous = prev
+
+    def setVisited(self):
+        self.visited = True
+
+    def __str__(self):
+        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+
+
+class Graph():
+    def __init__(self):
+        self.vertDictionary = {}
+        self.numVertices = 0
+
+    def __iter__(self):
+        return iter(self.vertDictionary.values())
+
+    def addVertex(self, node):
+        self.numVertices = self.numVertices + 1
+        newVertex = Vertex(node)
+        self.vertDictionary[node] = newVertex
+        return newVertex
+
+    def getVertex(self, n):
+        if n in self.vertDictionary:
+            return self.vertDictionary[n]
+        else:
+            return None
+
+    def addEdge(self, frm, to, cost=0):
+        if frm not in self.vertDictionary:
+            self.addVertex(frm)
+        if to not in self.vertDictionary:
+            self.addVertex(to)
+        self.vertDictionary[frm].addNeighbor(self.vertDictionary[to], cost)
+        # For directed graph do not add this
+        self.vertDictionary[to].addNeighbor(self.vertDictionary[frm], cost)
+
+    def getVertices(self):
+        return list(self.vertDictionary.keys())
+
+    def setPrevious(self, current):
+        self.previous = current
+
+    def getPrevious(self, current):
+        return self.previous
+
+    def getEdges(self):
+        edges = []
+        for v in G:
+            for w in v.getConnections():
+                vid = v.getVertexID()
+                wid = w.getVertexID()
+                edges.append((vid, wid, v.getWeight(w)))
+        return edges
+
+    def printGraph(self):
+        for v in self.vertDictionary:
+            print(self.vertDictionary[v])
+
+
+if __name__ == "__main__":
+    G = Graph()
+    G.addVertex('a')
+    G.addVertex('b')
+    G.addVertex('c')
+    G.addVertex('d')
+    G.addVertex('c')
+    G.addEdge('a', 'b', 4)
+    G.addEdge('a', 'c', 1)
+    G.addEdge('c', 'b', 2)
+    G.addEdge('b', 'e', 4)
+    G.addEdge('c', 'd', 4)
+    G.addEdge('d', 'e', 4)
+    print('Graph data:')
+    print(G.getEdges())
+    print(G.getVertices())
+    print("\nAdjacency List:")
+    G.printGraph()
---
+++ DataStructure.py/Graph_adjMATRIX.py
@@ -0,0 +1,97 @@
+"""
+    Adjacency Matrix Representation of graph.
+"""
+
+
+class Vertex:
+    def __init__(self, node):
+        self.id = node
+        # Mark all nodes unvisited
+        self.visited = False
+
+    def addNeighbor(self, neighbor, G):
+        G.addEdge(self.id, neighbor)
+
+    def getConnections(self, G):
+        return G.adjMatrix[self.id]
+
+    def getVertexID(self):
+        return self.id
+
+    def setVertexID(self, id):
+        self.id = id
+
+    def setVisited(self):
+        self.visited = True
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Graph:
+    def __init__(self, numVertices, cost=0):
+        self.adjMatrix = [[-1]*numVertices for _ in range(numVertices)]
+        self.numVertices = numVertices
+        self.vertices = []
+        for i in range(numVertices):
+            newVertex = Vertex(i)
+            self.vertices.append(newVertex)
+
+    def setVertex(self, vtx, id):
+        if 0 <= vtx < self.numVertices:
+            self.vertices[vtx].setVertexID(id)
+
+    def getVertex(self, n):
+        for vertex in range(self.numVertices):
+            if n == self.vertices[vertex].getVertexID():
+                return vertex
+        else:
+            return -1
+
+    def addEdge(self, frm, to, cost=0):
+        if self.getVertex(frm) != -1 and self.getVertex(to) != -1:
+            self.adjMatrix[self.getVertex(frm)][self.getVertex(to)] = cost
+            # for directed graph do not add this
+            self.adjMatrix[self.getVertex(to)][self.getVertex(frm)] = cost
+
+    def getVertices(self):
+        vertices = []
+        for vtxin in range(self.numVertices):
+            vertices.append(self.vertices[vtxin].getVertexID())
+        return vertices
+
+    def printMatrix(self):
+        for u in range(self.numVertices):
+            row = []
+            for v in range(self.numVertices):
+                row.append(self.adjMatrix[u][v])
+            print(row)
+
+    def getEdges(self):
+        edges = []
+        for v in range(self.numVertices):
+            for u in range(self.numVertices):
+                if self.adjMatrix[u][v] != -1:
+                    vid = self.vertices[v].getVertexID()
+                    wid = self.vertices[u].getVertexID()
+                    edges.append((vid, wid, self.adjMatrix[u][v]))
+        return edges
+
+
+if __name__ == "__main__":
+    G = Graph(5)
+    G.setVertex(0, 'a')
+    G.setVertex(1, 'b')
+    G.setVertex(2, 'c')
+    G.setVertex(3, 'd')
+    G.setVertex(4, 'e')
+    G.addEdge('a', 'e', 10)
+    G.addEdge('a', 'c', 20)
+    G.addEdge('c', 'b', 30)
+    G.addEdge('b', 'c', 40)
+    G.addEdge('e', 'd', 50)
+    G.addEdge('f', 'e', 60)
+    print('Graph data:')
+    print(G.printMatrix())
+    print(G.getEdges())
+    print(G.getVertices())
