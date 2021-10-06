2e05684ceec00e735a378be5fed27537b4171adb
Ishaan Jain <89084029+Catunoo@users.noreply.github.com>
2021-10-06 12:19:08 +0530

    Create trie.py
    Adding implementation code of trie in data structures.py

---
+++ DataStructure.py/trie.py
@@ -0,0 +1,75 @@
+class TrieNode:
+     
+    # Trie node class
+    def __init__(self):
+        self.children = [None]*26
+        self.isEndOfWord = False
+ 
+class Trie:
+     
+    # Trie data structure class
+    def __init__(self):
+        self.root = self.getNode()
+ 
+    def getNode(self):
+     
+        
+        return TrieNode()
+ 
+    def _charToIndex(self,ch):
+        
+        return ord(ch)-ord('a')
+ 
+ 
+    def insert(self,key):
+         
+        
+        pCrawl = self.root
+        length = len(key)
+        for level in range(length):
+            index = self._charToIndex(key[level])
+ 
+            # if current character is not present
+            if not pCrawl.children[index]:
+                pCrawl.children[index] = self.getNode()
+            pCrawl = pCrawl.children[index]
+ 
+        # mark last node as leaf
+        pCrawl.isEndOfWord = True
+ 
+    def search(self, key):
+            
+        pCrawl = self.root
+        length = len(key)
+        for level in range(length):
+            index = self._charToIndex(key[level])
+            if not pCrawl.children[index]:
+                return False
+            pCrawl = pCrawl.children[index]
+ 
+        return pCrawl.isEndOfWord
+ 
+
+def main():
+ 
+    # Input keys (use only 'a' through 'z' and lower case)
+    keys = ["the","a","there","anaswe","any",
+            "by","their"]
+    output = ["Not present in trie",
+              "Present in trie"]
+ 
+    # Trie object
+    t = Trie()
+ 
+    # Construct trie
+    for key in keys:
+        t.insert(key)
+ 
+    # Search for different keys
+    print("{} ---- {}".format("the",output[t.search("the")]))
+    print("{} ---- {}".format("these",output[t.search("these")]))
+    print("{} ---- {}".format("their",output[t.search("their")]))
+    print("{} ---- {}".format("thaw",output[t.search("thaw")]))
+ 
+
+main()
